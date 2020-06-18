f3c798963ca15775cb06dc720b071dba25a1f323
Kristian Correa <kristiangcorrea@live.com>
2020-06-18 16:39:46 -0400

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,24 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  return item.enhancement >= 20 ? { ...item } : { ...item, enhancement: item.enhancement + 1 };
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15) {
+    item.durability = item.durability - 5;
+  }
+  if(item.enhancement >= 15){
+    item.durability = item.durability - 10;
+  }
+  if(item.enhancement > 16){
+    item.enhancement = item.enhancement - 1;
+  }
+  return item;
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
